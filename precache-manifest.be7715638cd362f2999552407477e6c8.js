cdf73ce3c66c4d474d0abc70dbbbd01f01f180e9
Arnab Dutta <arnabduttasocial@gmail.com>
2021-09-04 16:44:21 +0530

    Updates

---
+++ precache-manifest.be7715638cd362f2999552407477e6c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2338806c17952e37bc1092ecefc512",
+    "url": "/crewmate-matching/index.html"
+  },
+  {
+    "revision": "fb0fb765ce06febf84fd",
+    "url": "/crewmate-matching/static/css/main.37e80007.chunk.css"
+  },
+  {
+    "revision": "0bb603e79cfffef4cf9a",
+    "url": "/crewmate-matching/static/js/2.90081d31.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/crewmate-matching/static/js/2.90081d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb0fb765ce06febf84fd",
+    "url": "/crewmate-matching/static/js/main.7f697a7d.chunk.js"
+  },
+  {
+    "revision": "ee5957d3316f3f0f9faf",
+    "url": "/crewmate-matching/static/js/runtime-main.9d7e70e9.js"
+  }
+]);
